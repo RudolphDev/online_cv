60f346cbdda391f5b1be9ab59edc8db19ae3f068
Thomas Renne <thomas.renne@u-psud.fr>
2021-06-23 22:38:11 +0200

    change lang switch method

--- scripts/language.js
+++ scripts/language.js
@@ -1,10 +1,27 @@
 $(function () {
     $('[lang]').hide(); // hide all lang attributes on start.
     $('[lang="fr"]').show(); // show just Korean text (you can change it)
-    console.log('First')
-    $('#lang-switch').change(function () { // put onchange event when user select option from select
-        var lang = $(this).val(); // decide which language to display using switch case. The rest is obvious (i think)
-        console.log('test')
+    console.log('First');
+});
+
+// Handle language menu
+function showww() {
+    document.getElementById("lang-select").style.height = "auto";
+    document.getElementById("lang-select").style.overflow = "y-scroll";
+}
+
+function hideee() {
+    document.getElementById("lang-select").style.height = "30px";
+    document.getElementById("lang-select").style.overflow = "hidden";
+  }
+
+
+function myfuunc(imgParent, lang) {
+  hideee();
+  var mainDIVV = document.getElementById("lang-select");
+  imgParent.parentNode.removeChild(imgParent);
+  mainDIVV.insertBefore(imgParent, mainDIVV.childNodes[0]);
+        console.log(lang);
         switch (lang) {
             case 'en':
                 $('[lang]').hide();
@@ -17,6 +34,5 @@ $(function () {
             default:
                 $('[lang]').hide();
                 $('[lang="fr"]').show();
-        }
-    });
-});
+        };
+}
