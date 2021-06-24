e66fa62abdab49a47ac288cc2c3dc09783aa815e
Thomas Renne <thomas.renne@u-psud.fr>
2021-06-24 18:34:43 +0200

    Finish lang menu, add job description in french

--- scripts/language.js
+++ scripts/language.js
@@ -8,31 +8,31 @@ $(function () {
 function showww() {
     document.getElementById("lang-select").style.height = "auto";
     document.getElementById("lang-select").style.overflow = "y-scroll";
+    document.getElementById("lang-select").style.border = "1px solid rgb(126, 135, 143);"
+    document.getElementById("lang-flag").style.display = "none"
 }
 
 function hideee() {
-    document.getElementById("lang-select").style.height = "30px";
+    document.getElementById("lang-select").style.height = "0px";
     document.getElementById("lang-select").style.overflow = "hidden";
+    document.getElementById("lang-flag").style.display = "block"
   }
 
 
-function myfuunc(imgParent, lang) {
-  hideee();
-  var mainDIVV = document.getElementById("lang-select");
-  imgParent.parentNode.removeChild(imgParent);
-  mainDIVV.insertBefore(imgParent, mainDIVV.childNodes[0]);
-        console.log(lang);
-        switch (lang) {
-            case 'en':
-                $('[lang]').hide();
-                $('[lang="en"]').show();
-                break;
-            case 'fr':
-                $('[lang]').hide();
-                $('[lang="fr"]').show();
-                break;
-            default:
-                $('[lang]').hide();
-                $('[lang="fr"]').show();
-        };
+function myfuunc(lang) {
+    hideee();
+    console.log(lang);
+    switch (lang) {
+        case 'en':
+            $('[lang]').hide();
+            $('[lang="en"]').show();
+            break;
+        case 'fr':
+            $('[lang]').hide();
+            $('[lang="fr"]').show();
+            break;
+        default:
+            $('[lang]').hide();
+            $('[lang="fr"]').show();
+    };
 }
