5e5cb22dfd9e32e98596a85c8334091d8b8f5996
Thomas Renne <thomas.renne@u-psud.fr>
2021-06-26 18:21:22 +0200

    full require bibtex

--- scripts/publications.js
+++ scripts/publications.js
@@ -1,5 +1,5 @@
-var bibtex = require('@scope/bibtex');
-
+var bibtex = require('../node_modules/bibtex/index.js');
+console.log("Test")
 const bibfile =bibtex.parseBibFile("../data/publis.bibtex");
 console.log(bibfile);
 
