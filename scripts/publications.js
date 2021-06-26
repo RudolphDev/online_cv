b10e57e7bd566c0660758df056961b43d71c16d1
Thomas Renne <thomas.renne@u-psud.fr>
2021-06-26 18:05:10 +0200

    print bibtex

--- scripts/publications.js
+++ scripts/publications.js
@@ -1,8 +1,9 @@
 import {bibtex} from "../node_modules";
 
+const bibfile =bibtex.parseBibFile("../data/publis.bibtex");
+console.log(bibfile);
 
 $(function () {
     // 
-    const bibfile =bibtex.parseBibFile("../data/publis.bibtex");
-    console.log(bibfile);
+    
 });
