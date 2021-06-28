5a117d1bb9f41c2cbe3f007bf985573536f0d782
Thomas Renne <thomas.renne@u-psud.fr>
2021-06-28 23:42:34 +0200

    include html methode

---
+++ scripts/publications.js
@@ -0,0 +1,15 @@
+$(function (){
+    var xhr= new XMLHttpRequest();
+    xhr.open('GET', '../data/publis.html', true);
+    console.log("Test");
+    xhr.onreadystatechange= function() {
+        console.log("Test");
+        if (this.readyState!==4) return;
+        if (this.status!==200) return; // or whatever error handling you want
+       
+        document.getElementById('includedContent').innerHTML= this.responseText;
+    };
+    xhr.send();
+    // document.getElementById("includedContent").innerHTML = "";
+    // var test = 
+});
