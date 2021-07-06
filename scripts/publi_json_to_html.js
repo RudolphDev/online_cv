0273fd2c9dc744a55db00755bf5e0ce87eb070e6
Thomas Renne <thomas.renne@u-psud.fr>
2021-07-06 19:11:40 +0200

    move menu inside title div

--- scripts/publi_json_to_html.js
+++ scripts/publi_json_to_html.js
@@ -1,9 +1,24 @@
 $(function() {
-    var people = [];
-    $.getJSON('../data/publis.json', function(data) {
+    $.getJSON('https://raw.githubusercontent.com/RudolphDev/online_cv/main/data/publis.json', function(data) {
         $.each(data, function(i, f) {
-           var tblRow = f.abstract;
-            $(tblRow).appendTo("#publication-entries");
+           var publidiv = "<div class=\"publi-div\">" + 
+           "<span class=\"publi-title\">" + f.title + "</span><br> " + 
+           "<span>";
+           $.each(f.author, function(i, author) {
+             if (author.family == "Renne" && author.given == "Thomas") {
+              publidiv = publidiv + "<b>" + author.given + " " + author.family + "</b>,";
+             }
+             if (i == Object.keys(f.author).length-1) {
+               console.log("Test")
+               publidiv = publidiv + "<span lang=\"en\"> and </span><span lang=\"fr\"> et </span>" + author.given + " " + author.family;
+             }
+             else {
+              publidiv = publidiv + author.given + " " + author.family + ",";
+             }
+           });
+           publidiv = publidiv + "</span><br><span>" + f["container-title"] + ", " + f.issued["date-parts"][0][0] + "</span>"
+           "</div>";
+            $(publidiv).appendTo("#publication-entries");
       });
  
     });
