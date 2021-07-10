f85d4302a4f855ad07f2a9ea4fcf9beece5d7852
Thomas Renne <thomas.renne@u-psud.fr>
2021-07-10 21:02:17 +0200

    Add front-page info

--- scripts/publi_json_to_html.js
+++ scripts/publi_json_to_html.js
@@ -5,22 +5,22 @@ $(function() {
            "<span class=\"publi-title\">" + f.title + "</span><br> " + 
            "<span>";
            $.each(f.author, function(i, author) {
-             if (author.family == "Renne" && author.given == "Thomas") {
-              publidiv = publidiv + "<b>" + author.given + " " + author.family + "</b>,";
-             }
-             if (i == Object.keys(f.author).length-1) {
-               console.log("Test")
-               publidiv = publidiv + "<span lang=\"en\"> and </span><span lang=\"fr\"> et </span>" + author.given + " " + author.family;
-             }
-             else {
-              publidiv = publidiv + author.given + " " + author.family + ",";
-             }
-           });
-           publidiv = publidiv + "</span><br><span>" + f["container-title"] + ", " + f.issued["date-parts"][0][0] + "</span>"
-           "</div>";
+              if (author.family == "Renne" && author.given == "Thomas") {
+                publidiv = publidiv + "<b>" + author.given + " " + author.family + "</b>,";
+              }
+              else {
+                if (i == Object.keys(f.author).length-1) {
+                  console.log("Test")
+                  publidiv = publidiv + "<span lang=\"en\"> and </span><span lang=\"fr\"> et </span>" + author.given + " " + author.family;
+                }
+                else {
+                  publidiv = publidiv + author.given + " " + author.family + ",";
+                }
+              }
+            });
+            publidiv = publidiv + "</span><br><span>" + f["container-title"] + ", " + f.issued["date-parts"][0][0] + "</span>"
+            "</div>";
             $(publidiv).appendTo("#publication-entries");
+          });
       });
- 
-    });
- 
- });
+  });
