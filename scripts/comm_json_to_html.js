ac3b5b98e3af0f255ac0f15a788c996649405e12
Thomas Renne <thomas.renne@u-psud.fr>
2022-08-21 17:12:35 -0400

    add all other comms

--- scripts/comm_json_to_html.js
+++ scripts/comm_json_to_html.js
@@ -9,6 +9,9 @@ function getFlag(country_name) {
     case "Canada":
       flag_code = "<span class=\"iconify\" data-icon=\"twemoji:flag-for-flag-canada\" data-inline=\"false\" data-width=\"24\" style=\"vertical-align:-0.25em\"></span>"
       break;
+    case "Europe":
+        flag_code = "<span class=\"iconify\" data-icon=\"twemoji:flag-for-flag-european-union\" data-inline=\"false\" data-width=\"24\" style=\"vertical-align:-0.25em\"></span>"
+        break;
     default:
       flag_code = ""
   }
@@ -18,13 +21,16 @@ function getFlag(country_name) {
 
 $(function() {
     $.getJSON('https://raw.githubusercontent.com/RudolphDev/online_cv/main/data/communications.json', function(data) {
+
         $.each(data, function(i, f) {
+          console.log(i)
+          console.log(f.city)
            var publidiv = "<div class=\"publi-div\"><div class=\"publi_cont_div\">" + 
            "<span class=\"publi-title\">" + f.title + "</span><br> ";
             publidiv = addAuthors(publidiv, f.author, f.id);
             publidiv = addAbstract(publidiv, f.abstract, f.id);
             publidiv = publidiv + "<div class=\"container\"><div class=\"publi-meta\"><b>" + f.congress + ", " + f.issued["date-parts"][0][0] + "</div>";
-            publidiv = publidiv + "<div style=\"float:right;\">" +   f.city + " " + getFlag(f.country) + "</div></b></div>";
+            publidiv = publidiv + "<div style=\"float:right;\">" +   f.city + ", " + getFlag(f.country) + "</div></b></div>";
             publidiv = publidiv + "</div>"
             publidiv = addMoreBtn(publidiv, f.id);
             publidiv = publidiv + "</div>"
